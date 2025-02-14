b3deebfc4057fa523fece53937a020629494eb4c
Hasnain <hacninetusar@gmail.com>
2025-02-14 14:45:26 +0600

    path added for getConversations

--- routes/conversationRoute.js
+++ routes/conversationRoute.js
@@ -1,11 +1,15 @@
 import express from 'express';
-import { createConversation, getMessages } from '../controllers/conversationController.js';
+import { createConversation, getConversations, getMessages } from '../controllers/conversationController.js';
 import { sendMessage } from '../controllers/messageController.js';
+import { acceptMessageRequest } from '../controllers/messageRequestController.js';
 
 const router = express.Router();
 
 router.post('/conversations', createConversation);
+router.get('/conversations/:userId', getConversations); 
 router.get('/conversations/:conversationId', getMessages);
 router.post('/messages/:conversationId', sendMessage);
+router.patch("/conversations/accept-request/:conversationId", acceptMessageRequest);
+
 
 export default router;
