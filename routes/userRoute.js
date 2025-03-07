2b6c330df4f3c9055569d1e428d8501388fcc12c
Hasnain <hacninetusar@gmail.com>
2025-03-07 08:38:15 +0600

    remove session storing fucntionality in userRoute

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,4 @@
 import express from "express";
-import bodyParser from "body-parser";
 import multer from "multer";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -12,31 +11,13 @@ import {
   getUserInfo,
 } from "../controllers/userController.js";
 import { isLogin, isLogout } from "../middlewares/auth.middleware.js";
-import session from "express-session";
-import dotenv from "dotenv";
-dotenv.config();
 
+// Initialize express router
 const userRouter = express.Router();
-const { SESSION_SECRET } = process.env;
-userRouter.use(
-  session({
-    secret: SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false,
-    cookie: {
-      secure: process.env.NODE_ENV === "production",
-      httpOnly: true,
-      maxAge: 3600000,
-    },
-  })
-);
 
 // Get __dirname equivalent in ES modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-userRouter.use(bodyParser.json());
-userRouter.use(bodyParser.urlencoded({ extended: true }));
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
